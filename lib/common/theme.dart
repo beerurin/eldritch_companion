4610f361d89ba1a251ae702ab720549138ed0c7e
Roman Zubin <utkabulka@gmail.com>
2024-06-18 17:44:26 +0400

    add inter font

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -3,17 +3,23 @@ Copyright (C) 2024 Roman Zubin
 
 Full notice can be found at /lib/main.dart file. */
 
+import 'package:eldritch_companion/common/constants.dart';
 import 'package:flutter/material.dart';
 
 final appTheme = ThemeData(
+  fontFamily: 'Inter',
   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
   useMaterial3: true,
   textTheme: const TextTheme(
     displayLarge: TextStyle(
-      fontFamily: 'Corben',
-      fontWeight: FontWeight.w700,
+      fontFamily: 'Inter',
       fontSize: 24,
       color: Colors.black,
     ),
   ),
+  buttonTheme: const ButtonThemeData(
+      minWidth: 100,
+      buttonColor: Colors.red,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(defaultCircularRadius))),
 );
