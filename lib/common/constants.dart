7f546af868bc408428f8e292b52e04ab304adc9e
Roman Zubin <utkabulka@gmail.com>
2024-06-20 12:45:11 +0400

    add notice about constants file

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -3,6 +3,12 @@ Copyright (C) 2024 Roman Zubin
 
 Full notice can be found at /lib/main.dart file. */
 
+// NOTICE!
+// This file can't have flutter dependencies, because data generator
+// program uses this file and it will break!
+//
+// All design constants go to theme.dart file!
+
 const String appName = 'Eldritch Companion';
 
 const String wikiSite = "eldritchhorror.fandom.com";
